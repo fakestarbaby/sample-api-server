c89737167d4b022a7c1ed53481862b08d2991c43
fakestarbaby <fakestarbaby@gmail.com>
2012-07-18 20:17:46 +0900

    Add rails new . -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApiServer::Application.config.secret_token = '47178ce52be7a6eefe9a768e53ed395dce5837383c8a51d70bdd2da4d53bda6a9e423d8e37425ac9e3b32c4c1e51dca0d2d1966bf191c6ff9363d6f233d5bd10'
